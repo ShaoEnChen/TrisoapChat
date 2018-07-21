85578f746a8fecc6e2ead8b090eb1449685c1593
ShaoEnChen <seanchen47@gmail.com>
2018-07-21 15:34:45 +0800

    put fixed string replies into one single file

---
+++ Dialog.js
@@ -0,0 +1,22 @@
+const Dialog = {
+	message: {
+		title: {
+			GET_STARTED: '您好！',
+			ASK_FOR_EMAIL: '給我信箱',
+			ASK_FOR_PHONE: '給我電話',
+			INVALID_EMAIL: '信箱格式不對啦',
+			INVALID_PHONE: '電話格式不對啦',
+			DATA_RECEIVED: '資料收到ㄌ',
+			FORWARD_TO_HUMAN_AGENT: '客服人員將會與您聯絡'
+		}
+	},
+	button: {
+
+	},
+	quick_reply: {
+		CUSTOMER_SERVICE: '洽詢客服',
+		WEDDING_PETITGIFT: '試用婚禮小物'
+	}
+};
+
+module.exports = Dialog;
