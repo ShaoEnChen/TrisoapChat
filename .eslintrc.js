d0f18a1af260f788c43b9a501dd2f9361c399191
ShaoEnChen <seanchen47@gmail.com>
2018-07-24 00:39:51 +0800

    remove eslintrc from ignore file

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true
+        }
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        // Below are custom rules
+        "no-mixed-spaces-and-tabs": [
+            "error",
+            "smart-tabs"
+        ]
+    }
+};
