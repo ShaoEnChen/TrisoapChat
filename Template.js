875d9793ea62a938e8e1870efaf972ad434461d5
ShaoEnChen <seanchen47@gmail.com>
2018-07-24 00:38:33 +0800

    merge code constructing Templates

--- Template.js
+++ Template.js
@@ -14,52 +14,173 @@ class Template {
 }
 
 class GenericTemplate extends Template {
-	constructor() {
+	constructor(/** Array<Element> */ elements) {
 		super();
 		const payload = this.attachment.payload;
 		payload.template_type = 'generic';
+		payload.elements = elements;
 	}
 
 	setImageAspectRatio(/** string(horizontal|square) */ image_aspect_ratio) {
 		this.attachment.payload.image_aspect_ratio = image_aspect_ratio;
 	}
 
-	setElements(/** Array<GenericElement> */ elements) {
-		this.attachment.payload.elements = elements;
-	}
+	// class GenericElement {
+	// 	constructor(/** string */ title) {
+	// 		this.title = title;
+	// 	}
+
+	// 	setSubtitle(/** string */ subtitle) {
+	// 		this.subtitle = subtitle;
+	// 	}
+
+	// 	setImageUrl(/** string */ image_url) {
+	// 		this.image_url = image_url;
+	// 	}
+
+	// 	setDefaultAction(/** DefaultAction */ default_action) {
+	// 		this.default_action = default_action;
+	// 	}
+
+	// 	setButtons(/** Array<Button> */ buttons) {
+	// 		this.buttons = buttons;
+	// 	}
+	// }
 }
 
 class MediaTemplate extends Template {
-	constructor() {
+	constructor(/** element */ element) {
 		super();
 		const payload = this.attachment.payload;
 		payload.template_type = 'media';
+		payload.elements = [element]; // Array<Element>{1}
 	}
 
-	setElements(/** Array<MediaElement{1}> */ elements) {
-		this.attachment.payload.elements = elements;
-	}
+	// class MediaElement {
+	// 	constructor(/** string(image|video) */ media_type,
+	// 			    /** boolean */ is_url,
+	// 			    /** string */ attachment) {
+	// 		this.media_type = media_type;
+
+	// 		if (is_url) {
+	// 			this.url = attachment;
+	// 		} else {
+	// 			this.attachment_id = attachment;
+	// 		}
+	// 	}
+
+	// 	setButtons(/** Array<Button> */ buttons) {
+	// 		this.buttons = buttons;
+	// 	}
+	// }
 }
 
 class ButtonTemplate extends Template {
-	constructor() {
+	constructor(/** string */ text, /** Array<Button> */ buttons) {
 		super();
 		const payload = this.attachment.payload;
 		payload.template_type = 'button';
+		payload.text = text;
+		payload.buttons = buttons;
+	}
+}
+
+function createTemplate(/** string(generic|media|button|quick_replies) */ templateType, ...params) {
+	switch(templateType) {
+	case 'generic':
+		return new GenericTemplate(...params);
+	case 'media':
+		return new MediaTemplate(...params);
+	case 'button':
+		return new ButtonTemplate(...params);
+	case 'quick_replies':
+		return new QuickReplies(...params);
+	default:
+		// unknown template type, error handling
+	}
+}
+
+class Button {
+	constructor(/** string */ type) {
+		this.type = type;
+	}
+}
+
+class DefaultAction extends Button {
+	constructor(/** string */ url) {
+		super('web_url');
+		this.url = url;
 	}
 
-	setText(/** string */ text) {
-		this.attachment.payload.text = text;
+	setWebviewHeightRatio(/** string(compact|tall|full) */ webview_height_ratio) {
+		this.webview_height_ratio = webview_height_ratio;
 	}
 
-	setButtons(/** Array<Button> */ buttons) {
-		this.attachment.payload.buttons = buttons;
+	setMessengerExtensions(/** boolean */ has_messenger_extensions) {
+		this.messenger_extensions = has_messenger_extensions;
+	}
+
+	setFallbackUrl(/** string */ fallback_url) {
+		this.fallback_url = fallback_url;
+	}
+
+	setWebviewShareButton(/** string(hide) */ webview_share_button) {
+		this.webview_share_button = webview_share_button;
+	}
+}
+
+class WebButton extends DefaultAction {
+	constructor(/** string */ title,
+			    /** string */ url) {
+		super(url);
+		this.title = title;
+	}
+}
+
+class PostbackButton extends Button {
+	constructor(/** string */ title,
+			    /** string */ payload) {
+		super('postback');
+		this.title = title;
+		this.payload = payload;
+	}
+}
+
+class ShareButton extends Button {
+	constructor(/** GenericTemplate */ share_contents) {
+		super('element_share');
+		this.share_contents = share_contents;
+	}
+}
+
+class PhoneButton extends Button {
+	constructor(/** string */ title,
+			    /** string */ phone) {
+		super('phone_number');
+		this.title = title;
+		this.payload = phone;
+	}
+}
+
+function createButton(/** string */ type, ...params) {
+	switch(type) {
+	case 'web':
+		return new WebButton(...params);
+	case 'postback':
+		return new PostbackButton(...params);
+	case 'share':
+		return new ShareButton(...params);
+	case 'phone':
+		return new PhoneButton(...params);
+	default:
+		// unknown button type, error handling
 	}
 }
 
 class QuickReplies {
 	setText(/** string */ text) {
 		this.text = text;
+		this.quick_replies = []; // Array<QuickReply>
 	}
 
 	setAttachment(/** Attachment */ attachment) {
@@ -71,9 +192,27 @@ class QuickReplies {
 	}
 }
 
+class QuickReply {
+	constructor(/** string(text|location|user_phone_number|user_email) */ content_type) {
+		this.content_type = content_type;
+	}
+
+	setTitle(/** string */ title) {
+		this.title = title;
+	}
+
+	setPayload(/** string */ payload) {
+		this.payload = payload;
+	}
+
+	setImageUrl(/** string */ image_url) {
+		this.image_url = image_url;
+	}
+}
+
 module.exports = {
-	GenericTemplate,
-	MediaTemplate,
-	ButtonTemplate,
-	QuickReplies
+	createTemplate,
+	createButton,
+	QuickReplies,
+	QuickReply
 };
